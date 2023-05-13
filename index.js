9456a937c72f795d07e937ca83c84262a28be7b1
TammyAlok2 <aloktamrakar2@gmail.com>
2023-05-13 17:16:02 +0530

    Mango db cluster commited successfully

--- index.js
+++ index.js
@@ -4,17 +4,32 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 3003;
+//passward = 3gEW0yLODDsWAoV1
+//const username = "Aloktam123";
+//const password = "<password>";
+//const cluster = "<cluster name>";
+//const dbname = "myFirstDatabase";
+//const uris =  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+//const uri = "mongodb+srv://Aloktam123:QxBiftJl36u85JkT@cluster0.5vq9abp.mongodb.net/?retryWrites=true&w=majority";
 const user =[];
 //mangodb connection
-mongoose
-  .connect('mongodb://127.0.0.1:27017/medistopdatabase', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('database connected'))  
-  .catch(e => console.log(e))
+//const urise = "mongodb+srv://amwaura89:password@cluster0.uim76jv.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Aloktam1234:3gEW0yLODDsWAoV1@cluster0.lhpfmkl.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set('strictQuery', false);
+async function connect(){
+    try {
+        await mongoose.connect(uri);
+        console.log("connected to MongoDB");
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+connect();
+ 
   
 
 const userSchema = new mongoose.Schema({
@@ -74,6 +89,9 @@ app.get("/schemes",(req,res)=>{
      app.get("/appointment",(req,res)=>{
       res.render('appoint')
     })
+    app.get('/information', (req,res) =>{
+      res.render('Information');
+    })
 
 
 app.post("/login", async (req, res) => {
@@ -118,8 +136,9 @@ app.post("/register", async (req, res) => {
     httpOnly: true,
     expires: new Date(Date.now() + 60 * 1000),
   });
-  res.redirect("/");
+  res.redirect("/information");
 });
+
 app.get("/logout", (req, res) => {
   res.cookie("token", null, {
     httpOnly: true,
